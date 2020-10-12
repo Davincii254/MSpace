d6376c648c49b31cb8a2af7bee30beff513bc49c
victor kuria <victorkuria7@gmail.com>
2020-10-12 17:56:51 +0300

    update list adapter

--- app/src/main/java/com/moringaschool/myspace/SecondActivity.java
+++ app/src/main/java/com/moringaschool/myspace/SecondActivity.java
@@ -2,13 +2,32 @@ package com.moringaschool.myspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.listView) TextView mSatelliteTextView;
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        ButterKnife.bind(this);
+
+        mSatelliteTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSatelliteTextView) {
+            Intent intent = new Intent(SecondActivity.this, SatelliteActivity.class);
+            startActivity(intent);
+        }
     }
 }
