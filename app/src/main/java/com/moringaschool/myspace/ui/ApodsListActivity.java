9a4c43444a7e9e982da2737a771b83c3a7aa052a
victor kuria <victorkuria7@gmail.com>
2020-11-02 18:10:18 +0300

    update ui

--- app/src/main/java/com/moringaschool/myspace/ui/ApodsListActivity.java
+++ app/src/main/java/com/moringaschool/myspace/ui/ApodsListActivity.java
@@ -58,7 +58,7 @@ public class ApodsListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_apods_list);
+        setContentView(R.layout.apods_list);
         ButterKnife.bind(this);
 
        final Intent intent = getIntent();
